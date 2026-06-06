9edfdbbb42315c7d3b08215fc96a47080e74dfac
liyang <yangyanglixiao@github.com>
2026-06-06 14:38:08 +0800

    feat: init frontend with Vite + Vue 3 + Element Plus + dependencies

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue'
+</script>
+
+<style>
+#app {
+  min-height: 100vh;
+  background: #f5f5f5;
+}
+</style>
